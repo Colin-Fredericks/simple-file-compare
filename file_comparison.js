ed7fb52bff119c35c22fad939fa0cb7fb459e8bf
Colin-Fredericks <Colin-Fredericks@users.noreply.github.com>
2026-08-14 11:40:41 -0400

    Plenty of refactoring

--- file_comparison.js
+++ file_comparison.js
@@ -1,6 +1,7 @@
 "use strict";
 console.log("working");
 let options_filename = document.currentScript.getAttribute("data-options");
+init();
 
 /** Create the file drop area and set up listeners. No parameters. */
 async function init() {
@@ -13,7 +14,7 @@ async function init() {
   }
   const options = window.file_comparison_options;
   displayMessage(
-    "Required files: " + options.correct_answers.join(", "),
+    "Required files: " + options.filenames.join(", "),
     "prompt-area",
     false,
   );
@@ -51,7 +52,6 @@ async function init() {
     fileInput.click();
   });
 }
-init();
 
 /**
  * Reads in the learner files and returns most of the info as an object.
@@ -92,28 +92,49 @@ async function readFiles(files, options) {
  * @param {*} options
  */
 async function compareFiles(all_file_content, options) {
-  if (Object.keys(all_file_content).length !== options.correct_answers.length) {
+  if (Object.keys(all_file_content).length !== options.filenames.length) {
     console.error("Did not upload all files.");
     displayMessage(
       "You uploaded " +
-        Object.keys(all_file_content).length +
-        " out of " +
-        options.correct_answers.length +
-        " required files. Please upload the required files.",
+      Object.keys(all_file_content).length +
+      " out of " +
+      options.filenames.length +
+      " required files. Please upload the required files.",
       "output-area",
       false,
     );
     return;
   }
 
+  let input_files = Object.keys(all_file_content);
+  let required_files = options.filenames.slice(); // Make a copy of the required filenames
   let max_credit = Object.keys(all_file_content).length;
   let current_credit = 0;
   let missing_required_word = options.must_have.map((x) => true); // Start with all required words missing
   let message = ""; // Will be reported to learner
 
-  for (const fileName in all_file_content) {
-    const f = all_file_content[fileName];
-    f.name = fileName;
+  for (const filename in all_file_content) {
+    const f = all_file_content[filename];
+    f.name = filename;
+
+    // Is this a file we wanted?
+    if (!options.filenames.includes(f.name)) {
+      console.error("Unexpected file: " + f.name);
+      displayMessage(
+        "Unexpected file: " + f.name + ". Please upload the required files.",
+        "output-area",
+        true,
+      );
+      continue;
+    } else {
+      // Remove from list so we can keep track of which files have been processed.
+      let index = required_files.indexOf(f.name);
+      if (index > -1) {
+        required_files.splice(index, 1);
+      }
+    }
+
+
     let this_file_credit = 1;
     let apply_partial_credit = {
       blank_lines: false,
@@ -129,9 +150,7 @@ async function compareFiles(all_file_content, options) {
     ) {
       // This is not a text file.
       let outputArea = document.querySelector("#output-area");
-      outputArea.innerHTML += "<p>" + f.name + " is not a text file.</p>";
-      console.log(f.name);
-      console.log(f.type);
+      outputArea.innerHTML += "<p>" + f.name + " is of type " + f.type + ", not a text file.</p>";
     } else {
       // Yay it's a text file!
       displayMessage("Filename: " + f.name, "output-area", true);
@@ -144,19 +163,21 @@ async function compareFiles(all_file_content, options) {
 
       // Using hashes if you want to avoid revealing the correct answer
       if (options.files_or_hashes === "hashes") {
-        // Hash the correct file content and the submitted file content.
-        let correct_file_hash = options.correct_answers[fileName];
+        // Hash the submitted file's content.
         let submitted_file_hash = await sha256(f.content);
-        if (correct_file_hash === submitted_file_hash) {
-          console.log("Hashes match for " + f.name);
-          continue;
+        let msg = "";
+        if (options.hashes[filename] === submitted_file_hash) {
+          msg = "Hashes match for " + f.name + ".\n";
         } else {
+          msg = "Hashes do not match for " + f.name + ". No credit for this file.\n";
           this_file_credit = 0;
-          continue;
         }
+        console.log(msg);
+        message += msg;
+        continue;
       }
 
-      // Keys for the `credit_options` object. All are Numbers.
+      // Just for reference, these are the keys for the `credit_options` object. All are Numbers.
       //   blank_lines
       //   case
       //   spaces
@@ -164,8 +185,6 @@ async function compareFiles(all_file_content, options) {
       //   high_cutoff
       //   participation_points
 
-      // console.log('Correct file content:');
-      // console.log(correct_file_content);
       let submitted_file_content = f.content;
       let correct_by_line = correct_file_content.split("\n");
       let submitted_by_line = submitted_file_content.split("\n");
@@ -194,11 +213,10 @@ async function compareFiles(all_file_content, options) {
           }
         }
 
-        // console.log('Comparing line ' + (i + 1));
         if (i + offset >= submitted_by_line.length) {
           console.log("Ran out of lines in submitted file.");
           message +=
-            "Ran out of lines in " + f.name + ". No credit for this file.\n";
+            f.name + " is the wrong length. No credit for this file.\n";
           break;
         }
         if (correct_by_line[i] === submitted_by_line[i + offset]) {
@@ -207,67 +225,41 @@ async function compareFiles(all_file_content, options) {
           continue;
         }
 
-        if (correct_by_line[i] === "" && submitted_by_line[i + offset] !== "") {
-          // The correct file has a blank line, but the submitted file does not.
-          // Hold back our count on the submitted file by one line.
-          console.log(
-            "Holding back one line at " +
-              (i + 1) +
-              " in the submitted file because the correct file has a blank line.",
-          );
+        // Checking for blank lines in either file and adjusting the offset accordingly.
+        let old_offset = offset;
+        offset = correctTheOffset(correct_by_line, submitted_by_line, i, offset);
+        if (offset !== old_offset) {
           apply_partial_credit.blank_lines = true;
-          offset--;
-          continue;
-        } else if (
-          correct_by_line[i] !== "" &&
-          submitted_by_line[i + offset] === ""
-        ) {
-          // The submitted file has a blank line, but the correct file does not.
-          // Move forward the line we're examining in the submitted file by one line.
-          console.log(
-            "Moving forward one line at " +
-              (i + 1) +
-              " in the submitted file because the submitted file has a blank line.",
-          );
-          apply_partial_credit.blank_lines = true;
-          offset++;
-          continue;
+          continue; // Skip the rest of the checks for this line since we adjusted for blank lines.
         }
 
         // Imperfect match, check for partial credit.
-        let cl = correct_by_line[i].trim();
-        let sl = submitted_by_line[i + offset].trim();
-        if (cl !== sl) {
-          // Case checking
-          if (cl.toLowerCase() === sl.toLowerCase()) {
-            console.log("Line " + (i + 1) + " is the same except for case.");
-            apply_partial_credit.case = true;
-          } else {
-            console.log(
-              "Line " + (i + 1) + " is entirely different. Done comparing.",
-            );
-            message +=
-              "Line " +
-              (i + 1) +
-              " is entirely different in " +
-              f.name +
-              ". No credit for this file.\n";
-            this_file_credit = 0;
-            break;
-          }
-        } else {
-          // Whitespace checking
-          console.log(
-            "Line " +
-              (i + 1) +
-              " matches except for whitespace at start or end.",
-          );
+        if (matchesWithoutCase(correct_by_line[i], submitted_by_line[i + offset])) {
+          console.log("Line " + (i + 1) + " is the same except for case.");
+          apply_partial_credit.case = true;
+        } else if (matchesWithoutWhitespace(correct_by_line[i], submitted_by_line[i + offset])) {
+          console.log("Line " + (i + 1) + " matches except for whitespace at start or end.");
           apply_partial_credit.spaces = true;
+        } else if (matchesWithoutCaseAndWhitespace(correct_by_line[i], submitted_by_line[i + offset])) {
+          console.log("Line " + (i + 1) + " matches except for case and whitespace at start or end.");
+          apply_partial_credit.case = true;
+          apply_partial_credit.spaces = true;
+        } else {
+          console.log(
+            "Line " + (i + 1) + " is entirely different. Done comparing.",
+          );
+          message +=
+            "Line " +
+            (i + 1) +
+            " is entirely different in " +
+            f.name +
+            ". No credit for this file.\n";
+          this_file_credit = 0;
+          break;
         }
       }
-      console.log("Match by line for " + f.name + ":");
-      console.log(match_by_line);
     }
+
     for (const key in apply_partial_credit) {
       if (apply_partial_credit[key]) {
         console.log("Partial credit applied for " + key);
@@ -278,39 +270,54 @@ async function compareFiles(all_file_content, options) {
       console.log("Missing required word(s) in " + f.name);
       this_file_credit = 0;
     }
+
+    // If we're not applying any partial credit, this is a perfect match.
+    // Otherwise, explain why.
+    let applying_partial_credit = Object.values(apply_partial_credit).every(x => x === false);
+    if (!applying_partial_credit && this_file_credit === 1) {
+      message += "Perfect match for " + f.name + ".\n";
+    } else {
+      if (this_file_credit > 0) {
+        message += "Partial match for " + f.name + ".\n";
+      } else {
+        message += "Insufficient match for " + f.name + ".\n";
+      }
+      message += partialCreditMessage(options, apply_partial_credit);
+    }
+
     this_file_credit = Math.round(this_file_credit * 100) / 100; // Round to two decimal places
     current_credit += this_file_credit;
     console.log(
       "Credit for " + f.name + ": " + decimalToPercentage(this_file_credit),
     );
 
-    if (this_file_credit > 0) {
-      if (options.credit_options.spaces && apply_partial_credit.spaces) {
-        message +=
-          "Partial credit for whitespace: " +
-          decimalToPercentage(options.credit_options.spaces) +
-          "\n";
-      }
-      if (options.credit_options.case && apply_partial_credit.case) {
-        message +=
-          "Partial credit for case: " +
-          decimalToPercentage(options.credit_options.case) +
-          "\n";
-      }
-      if (
-        options.credit_options.blank_lines &&
-        apply_partial_credit.blank_lines
-      ) {
-        message +=
-          "Partial credit for blank lines: " +
-          decimalToPercentage(options.credit_options.blank_lines) +
-          "\n";
-      }
-    }
   }
+
+  /**********************************
+  Final credit calculation section
+  **********************************/
   let credit = current_credit / max_credit;
+  let msg = "";
+  if (credit < options.credit_options.low_cutoff) {
+    credit = 0;
+    msg = "Credit is below " + decimalToPercentage(options.credit_options.low_cutoff) + ". No credit awarded.\n";
+    message += msg;
+    console.log(msg);
+  }
+  if (credit + options.credit_options.participation_points <= 1) {
+    credit += options.credit_options.participation_points;
+    msg = "Adding participation points: +" + decimalToPercentage(options.credit_options.participation_points) + "\n";
+    message += msg;
+    console.log(msg);
+  }
+  if (credit > options.credit_options.high_cutoff && credit < 1) {
+    credit = 1;
+    msg = "Credit is above " + decimalToPercentage(options.credit_options.high_cutoff) + ". Rounding up to full credit.\n";
+    message += msg;
+    console.log(msg);
+  }
   console.log(
-    "Final credit: " + decimalToPercentage(current_credit / max_credit),
+    "Final credit: " + decimalToPercentage(credit),
   );
   message += "Final credit: " + decimalToPercentage(credit) + "\n";
   displayMessage(message, "output-area", true);
@@ -326,6 +333,89 @@ async function getOptions() {
   console.log(options);
   return options;
 }
+
+/** Check for whitespace mismatch */
+function matchesWithoutWhitespace(str1, str2) {
+  // Remove leading and trailing whitespace from both strings
+  const trimmedStr1 = str1.trim();
+  const trimmedStr2 = str2.trim();
+
+  // Compare the trimmed strings
+  return trimmedStr1 === trimmedStr2;
+}
+
+/** Check if two strings match regardless of case */
+function matchesWithoutCase(str1, str2) {
+  return str1.toLowerCase() === str2.toLowerCase();
+}
+
+/** Does both case and whitespace */
+function matchesWithoutCaseAndWhitespace(str1, str2) {
+  // Remove leading and trailing whitespace from both strings
+  const trimmedStr1 = str1.trim();
+  const trimmedStr2 = str2.trim();
+
+  // Compare the trimmed strings in lowercase
+  return trimmedStr1.toLowerCase() === trimmedStr2.toLowerCase();
+}
+
+/** Adjust for blank lines */
+function correctTheOffset(correct_by_line, submitted_by_line, i, offset) {
+  if (correct_by_line[i] === "" && submitted_by_line[i + offset] !== "") {
+    // The correct file has a blank line, but the submitted file does not.
+    // Hold back our count on the submitted file by one line.
+    console.log(
+      "Holding back one line at " +
+      (i + 1) +
+      " in the submitted file because the correct file has a blank line.",
+    );
+    offset--;
+  } else if (
+    correct_by_line[i] !== "" &&
+    submitted_by_line[i + offset] === ""
+  ) {
+    // The submitted file has a blank line, but the correct file does not.
+    // Move forward the line we're examining in the submitted file by one line.
+    console.log(
+      "Moving forward one line at " +
+      (i + 1) +
+      " in the submitted file because the submitted file has a blank line.",
+    );
+    offset++;
+  }
+  return offset;
+}
+
+/** Assembles the message for partial credit (per file) */
+function partialCreditMessage(options, apply_partial_credit) {
+
+  let message = "";
+
+  if (options.credit_options.spaces < 1 && apply_partial_credit.spaces) {
+    message +=
+      "Partial credit for whitespace: x" +
+      decimalToPercentage(options.credit_options.spaces) +
+      "\n";
+  }
+  if (options.credit_options.case < 1 && apply_partial_credit.case) {
+    message +=
+      "Partial credit for case: x" +
+      decimalToPercentage(options.credit_options.case) +
+      "\n";
+  }
+  if (
+    options.credit_options.blank_lines < 1 &&
+    apply_partial_credit.blank_lines
+  ) {
+    message +=
+      "Partial credit for blank lines: x" +
+      decimalToPercentage(options.credit_options.blank_lines) +
+      "\n";
+  }
+
+  return message;
+}
+
 
 /**
  * Displays a message in the specified area.
@@ -377,5 +467,7 @@ async function sha256(source) {
   const sourceBytes = new TextEncoder().encode(source);
   const digest = await crypto.subtle.digest("SHA-256", sourceBytes);
   const resultBytes = [...new Uint8Array(digest)];
-  return resultBytes.map((x) => x.toString(16).padStart(2, "0")).join("");
+  const hash = resultBytes.map((x) => x.toString(16).padStart(2, "0")).join("");
+  console.log("SHA256 hash: " + hash);
+  return hash;
 }
